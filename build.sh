c2a338ec283cc8887c79c17b2898b8abb2a5b80d
Yuri Andri Gani <yuri@inovasiriset.co.id>
2026-01-29 13:16:15 +0700

    feat: Add robust argument parsing with a help message to `build.sh`.

--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
-# !/bin/bash
-if [[ "$*" == *"--ui"* ]]; then
-  ./view.sh
-fi
+#!/bin/sh
+set -e
+
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  --ui    Run UI"
+  echo "  --help  Show this help message"
+  exit 1
+}
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    --ui)
+      ./view.sh
+      shift
+      ;;
+    --help|-h)
+      usage
+      ;;
+    *)
+      echo "Unknown option: $1"
+      usage
+      ;;
+  esac
+done
+
 CGO_ENABLED=0 go build -o ./dist/mandok main.go
--- view.sh
+++ view.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 go tool templ generate
 cd nodejs && npx tailwindcss -i ../static/input.css -o ../static/main.css && cd ..
